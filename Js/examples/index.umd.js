d9ba7b3d78a63f86112a75cf719f29a4344489ca
Twittytop <hongwei.tu@chukong-inc.com>
2019-12-05 19:03:51 +0800

    chore(*): up

--- Js/examples/index.umd.js
+++ Js/examples/index.umd.js
@@ -4,10 +4,26 @@
  * @license MIT
  */
 (function (factory) {
-	typeof define === 'function' && define.amd ? define(factory) :
-	factory();
+  typeof define === 'function' && define.amd ? define(factory) :
+  factory();
 }((function () { 'use strict';
 
+  window.JSBridge = {};
 
+  window.JSBridge.callJS = function (_ref) {
+    var action = _ref.action,
+        params = _ref.params,
+        whoAmI = _ref.whoAmI;
+    params = JSON.parse(JSON.stringify(params));
+    return window.JSBridge[action](params, whoAmI);
+  };
+
+  JSBridge.canBack = function (params, whoAmI) {
+    var result = {
+      can: false,
+      target: 'prev'
+    };
+    return result;
+  };
 
 })));
